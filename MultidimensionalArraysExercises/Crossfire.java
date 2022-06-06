ebf800fbc4d8ca4040f5eba20e377f7c3e8c9bc0
Georgi Bangeev <bangeev13@gmail.com>
2022-06-06 15:16:21 +0300

    Update Crossfire.java

--- MultidimensionalArraysExercises/Crossfire.java
+++ MultidimensionalArraysExercises/Crossfire.java
@@ -1,109 +1,77 @@
 package MultidimensionalArraysExercises;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class p06StringMatrixRotation {
+public class p07Crossfire {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
-        Pattern pattern = Pattern.compile("\\d+");
-        Matcher matcher = pattern.matcher(command);
-        int degree = 0;
-        while (matcher.find()){
-            degree = Integer.parseInt(matcher.group());
-        }
+
+        int[] size = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int rows = size[0];
+        int cols = size[1];
+
+        List<List<Integer>> matrix = new ArrayList<>();
+
+        fillListMatrix(rows, cols, matrix);
 
         String line;
-//get string for the initial matrix
-        List<String> input = new ArrayList<>();
-        while (!"END".equals(line = scanner.nextLine())) {
-            input.add(line);
-        }
-//calculate cal colon length
-        int cols = input.stream()
-                .mapToInt(e -> e.length())
-                .max().orElse(0);
-//populate initial matrix
-        char[][] matrix = new char[input.size()][cols];
-        for (int i = 0; i < input.size(); i++) {
-            while (input.get(i).length() < cols) {
-                input.set(i, input.get(i) + " ");
-            }
-            matrix[i] = input.get(i).toCharArray();
-        }
-//calculate rotation index
-        int rotate = degree / 90;
-        while (rotate >= 4) {
-            rotate -=4;
-        }
-//print correctly rotated matrix
-        if (rotate == 1) {
-            printMatrix(rotate90(matrix));
-        } else if (rotate == 2) {
-            printMatrix(rotate180(matrix));
-        } else if (rotate == 3) {
-            printMatrix(rotate270(matrix));
-        } else {
-            printMatrix(matrix);
-        }
+        while (!"Nuke it from orbit".equals(line = scanner.nextLine())) {
+            int[] input = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+            int r = input[0];
+            int c = input[1];
+            int radius = input[2];
 
+            nukeTheTarget(matrix, r, c, radius);
+            removeEmptyLines(matrix);
+        }
+        printMatrix(matrix);
     }
 
-    private static char[][] rotate270(char[][] matrix) {
-        char[][] newMatrix = new char[matrix[0].length][matrix.length];
-        int r = 0;
-        int c = matrix[0].length - 1;
-        for (int i = 0; i < newMatrix.length; i++) {
-            for (int j = 0; j < newMatrix[i].length; j++) {
-                newMatrix[i][j] = matrix[r][c];
-                r++;
+    private static void fillListMatrix(int rows, int cols, List<List<Integer>> matrix) {
+        int counter = 1;
+        for (int i = 0; i < rows; i++) {
+            matrix.add(new ArrayList<>());
+            for (int j = 0; j < cols; j++) {
+                matrix.get(i).add(counter++);
             }
-            c--;
-            r = 0;
         }
-        return newMatrix;
     }
 
-    private static char[][] rotate180(char[][] matrix) {
-        char[][] newMatrix = new char[matrix.length][matrix[0].length];
-        int r = matrix.length - 1;
-        int c = matrix[0].length - 1;
-        for (int i = 0; i < newMatrix.length; i++) {
-            for (int j = 0; j < newMatrix[i].length; j++) {
-                newMatrix[i][j] = matrix[r][c];
-                c--;
+    private static void removeEmptyLines(List<List<Integer>> matrix) {
+        for (int i = 0; i < matrix.size(); i++) {
+            if (matrix.get(i).size() == 0) {
+                matrix.remove(i--);
             }
-            r--;
-            c = matrix[0].length - 1;
         }
-        return newMatrix;
     }
 
-    private static void printMatrix(char[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j]);
+    private static void printMatrix(List<List<Integer>> matrix) {
+        for (List<Integer> list : matrix) {
+            for (Integer integer : list) {
+                System.out.print(integer + " ");
             }
             System.out.println();
         }
     }
 
-    private static char[][] rotate90(char[][] matrix) {
-        char[][] newMatrix = new char[matrix[0].length][matrix.length];
-        int r = matrix.length - 1;
-        int c = 0;
-        for (int i = 0; i < newMatrix.length; i++) {
-            for (int j = 0; j < newMatrix[i].length; j++) {
-                newMatrix[i][j] = matrix[r][c];
-                r--;
+    private static void nukeTheTarget(List<List<Integer>> matrix, int r, int c, int radius) {
+        //remove nuked row
+        for (int i = c + radius; i >= c - radius; i--) {
+            if (isValidIndex(r, matrix.size()) && isValidIndex(i, matrix.get(r).size())) {
+                matrix.get(r).remove(i);
             }
-            c++;
-            r = matrix.length - 1;
         }
-        return newMatrix;
+        //remove nuked column except for row r
+        for (int i = r - radius; i <= r + radius; i++) {
+            if (isValidIndex(i, matrix.size()) && isValidIndex(c, matrix.get(i).size()) && i != r) {
+                matrix.get(i).remove(c);
+            }
+        }
+    }
+
+    private static boolean isValidIndex(int i, int size) {
+        return i >= 0 && i < size;
     }
 }
