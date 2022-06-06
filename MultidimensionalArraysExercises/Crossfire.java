94a5ef4bf06e69a0566ab1c60159b58e67904c04
Georgi Bangeev <bangeev13@gmail.com>
2022-06-06 15:14:30 +0300

    Update Crossfire.java

--- MultidimensionalArraysExercises/Crossfire.java
+++ MultidimensionalArraysExercises/Crossfire.java
@@ -1,77 +1,109 @@
 package MultidimensionalArraysExercises;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Crossfire {
-
+public class p06StringMatrixRotation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //вход
-        int[] dimensions = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
-        int rows = dimensions[0];
-        int cols = dimensions[1];
-
-
-        List<List<Integer>> matrix = new ArrayList<>();
-        fillMatrix(matrix, rows, cols);
-
         String command = scanner.nextLine();
-        while (!command.equals("Nuke it from orbit")) {
-            String[] tokens = command.split("\\s+"); // 3 4 1
-            int row = Integer.parseInt(tokens[0]);
-            int col = Integer.parseInt(tokens[1]); //индексът на елемента в листа
-            int radius = Integer.parseInt(tokens[2]);
-
-            //destroy up, down
-            for (int currentRow = row - radius; currentRow <= row + radius ; currentRow++) {
-                if(isInMatrix(currentRow, col, matrix)) { //валидираме реда от който ще вземем
-                    matrix.get(currentRow).remove(col);
-                }
-            }
-
-            //destroy left. right
-            for (int currentColumn = col + radius; currentColumn >-  col - radius; currentColumn--) {
-                if(isInMatrix(row, currentColumn, matrix)) { //валидираме колоната от който ще вземем
-                    matrix.get(row).remove(currentColumn);
-                }
-            }
-
-            matrix.removeIf(List::isEmpty);
-
-
-            command = scanner.nextLine();
-
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(command);
+        int degree = 0;
+        while (matcher.find()){
+            degree = Integer.parseInt(matcher.group());
         }
 
-        printMatrix(matrix);
-    }
-
-    private static boolean isInMatrix(int row, int col, List<List<Integer>> matrix) {
-        return row >= 0 && row < matrix.size() && col >= 0 && col < matrix.get(row).size();
-    }
-
-
-    //fill matrix
-    private static void fillMatrix(List<List<Integer>> matrix, int rows, int cols) {
-        int number = 1;
-        for (int row = 0; row < rows; row++) {
-            matrix.add(new ArrayList<>());
-            for (int col = 0; col < cols; col++) {
-                matrix.get(row).add(number++);
-            }
+        String line;
+//get string for the initial matrix
+        List<String> input = new ArrayList<>();
+        while (!"END".equals(line = scanner.nextLine())) {
+            input.add(line);
         }
+//calculate cal colon length
+        int cols = input.stream()
+                .mapToInt(e -> e.length())
+                .max().orElse(0);
+//populate initial matrix
+        char[][] matrix = new char[input.size()][cols];
+        for (int i = 0; i < input.size(); i++) {
+            while (input.get(i).length() < cols) {
+                input.set(i, input.get(i) + " ");
+            }
+            matrix[i] = input.get(i).toCharArray();
+        }
+//calculate rotation index
+        int rotate = degree / 90;
+        while (rotate >= 4) {
+            rotate -=4;
+        }
+//print correctly rotated matrix
+        if (rotate == 1) {
+            printMatrix(rotate90(matrix));
+        } else if (rotate == 2) {
+            printMatrix(rotate180(matrix));
+        } else if (rotate == 3) {
+            printMatrix(rotate270(matrix));
+        } else {
+            printMatrix(matrix);
+        }
+
     }
 
-    //print matrix
-    public static void printMatrix(List<List<Integer>> matrix) {
-        for (List<Integer> row : matrix) {
-            for (int element : row ) {
-                System.out.print(element + " ");
+    private static char[][] rotate270(char[][] matrix) {
+        char[][] newMatrix = new char[matrix[0].length][matrix.length];
+        int r = 0;
+        int c = matrix[0].length - 1;
+        for (int i = 0; i < newMatrix.length; i++) {
+            for (int j = 0; j < newMatrix[i].length; j++) {
+                newMatrix[i][j] = matrix[r][c];
+                r++;
+            }
+            c--;
+            r = 0;
+        }
+        return newMatrix;
+    }
+
+    private static char[][] rotate180(char[][] matrix) {
+        char[][] newMatrix = new char[matrix.length][matrix[0].length];
+        int r = matrix.length - 1;
+        int c = matrix[0].length - 1;
+        for (int i = 0; i < newMatrix.length; i++) {
+            for (int j = 0; j < newMatrix[i].length; j++) {
+                newMatrix[i][j] = matrix[r][c];
+                c--;
+            }
+            r--;
+            c = matrix[0].length - 1;
+        }
+        return newMatrix;
+    }
+
+    private static void printMatrix(char[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]);
             }
             System.out.println();
         }
+    }
+
+    private static char[][] rotate90(char[][] matrix) {
+        char[][] newMatrix = new char[matrix[0].length][matrix.length];
+        int r = matrix.length - 1;
+        int c = 0;
+        for (int i = 0; i < newMatrix.length; i++) {
+            for (int j = 0; j < newMatrix[i].length; j++) {
+                newMatrix[i][j] = matrix[r][c];
+                r--;
+            }
+            c++;
+            r = matrix.length - 1;
+        }
+        return newMatrix;
     }
 }
